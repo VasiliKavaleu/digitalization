ddd896a964cd3027e07fbac29a8bafe8750db4e0
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2020-09-13 00:52:45 +0300

    updated settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
